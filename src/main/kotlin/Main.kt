25f26d226cc2dfd7bcec5ddd2e3530e8b02a30dd
JonathanUlhaq <vianulhag@gmail.com>
2023-09-15 14:55:53 +0700

    Testing

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+fun main() {
+    val user = setUser("Joko",21)
+    println(user)
+}
+
+fun setUser(nama:String,umur:Int):String {
+    return "$nama $umur"
+}
+
+fun setName(nama:String,umur:Int):String = "$nama $umur"
+
+
+
